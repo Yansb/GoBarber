bbf2c2636f6f8775aa45d9d216179dea8d026103
yansb <yansbarreiro@gmail.com>
2020-05-15 03:55:36 -0300

    :card_file_box:Adicionado banco de dados Redis e memoria Cache

--- src/modules/users/repositories/IUserTokensRepository.ts
+++ src/modules/users/repositories/IUserTokensRepository.ts
@@ -1,6 +1,7 @@
+import { Secret } from 'jsonwebtoken';
 import UserToken from '../infra/typeorm/entities/UserToken';
 
 export default interface IUserTokensRepository {
-  generate(user_id: string): Promise<UserToken>;
+  generate(user_id: Secret): Promise<UserToken>;
   findByToken(token: string): Promise<UserToken | undefined>;
 }
