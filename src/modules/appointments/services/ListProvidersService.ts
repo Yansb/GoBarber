498f0b62183a617dbd5306e94ae68e97c02b07b3
Yan Santana <yansbarreiro@gmail.com>
2020-06-15 17:25:45 -0300

    :card_file_box: usando o ClassToClass com os providers

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   user_id: string;
@@ -22,13 +23,20 @@ export default class ListProvidersService {
     let users = await this.cacheProvider.recover<User[]>(
       `providers-list:${user_id}`,
     );
+
+    // para desabilitar o cache basta descomentar esse users e comentar o de cima
+    // let users = null;
+
     if (!users) {
       users = await this.usersRepository.findAllProviers({
         except_user_id: user_id,
       });
     }
 
-    await this.cacheProvider.save(`providers-list:${user_id}`, users);
+    await this.cacheProvider.save(
+      `providers-list:${user_id}`,
+      classToClass(users),
+    );
 
     return users;
   }
